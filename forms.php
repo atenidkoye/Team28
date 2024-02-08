dc29d2594133e01ba2b9015195b48b239e757515
Nelson Njoku <113510892+Acoolwolf96@users.noreply.github.com>
2024-02-08 23:03:25 +0200

    added layout, css and payment form

--- forms.php
+++ forms.php
@@ -3,9 +3,35 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <link rel="stylesheet" href="style.css">
+    <title>Payment Form</title>
+    
 </head>
 <body>
-    <h1>hello forms</h1>
+    <div class="container">
+        <h2>Secure Payment</h2>
+        <form action="#" method="post">
+
+            <label for="name">first Name:</label>
+            <input type="text" id="name" name="name" required>
+
+            <label for="name">Last Name:</label>
+            <input type="text" id="name" name="name" required>
+
+            <label for="email">Email:</label>
+            <input type="email" id="email" name="email" required>
+
+            <label for="card Number">Card Number</label>
+            <input type="Number" id="number" name="Number" required>
+
+            <label for="date">Expiry Date</label>
+            <input type="date" id="name" name="date" required>
+
+            <label for="CCV">CCV</label>
+            <input type="Number" id="name" name="Number" required>
+            
+        </form>
+    </div>
+
 </body>
 </html>
