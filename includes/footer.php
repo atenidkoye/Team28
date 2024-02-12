233c8dd01b93dcc0c7f1769278f7f88b1fcd1e6d
Nelson Njoku <113510892+Acoolwolf96@users.noreply.github.com>
2024-02-12 11:05:05 +0200

    updated files

---
+++ includes/footer.php
@@ -0,0 +1,22 @@
+<footer>
+<section class="about" id="about">
+        <h1 class="heading"><span>about</span> us</h1>
+        <div class="new">
+            <div class="img-container">
+                <img src="images/pexels-татьяна-чернышова🍒-3914589.jpg">
+                <!-- <h3>We are TEAM28, best flower sellers</h3> -->
+            </div>
+            
+
+            <div class="content">
+                <h3>WHY US</h3>
+                <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ducimus, dolores vel. Ipsam laboriosam asperiores voluptatem magnam, similique consectetur unde maxime perspiciatis nemo dolores modi suscipit possimus iusto excepturi quo officiis.</p>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Veritatis corrupti facere autem commodi. Saepe nemo consequuntur esse, voluptatem voluptas ducimus sapiente possimus deleniti veniam ipsam, provident dolorum delectus odio itaque.</p>
+                <a href="#" class="btn">Click Here</a>
+
+            </div>
+        </div>
+    </section>
+</footer>
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,88 @@
+<?php
+include 'includes/header.php';
+?>
+    <section class="home" id="home">
+            <div class="content">
+                <h3>Fresh Flowers</h3>
+                <span>Natural Flowers</span>
+                <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Laborum obcaecati voluptatem reiciendis atque optio, beatae dolores ea totam amet itaque, eaque ratione illum tempore inventore enim voluptates ducimus alias maxime.</p>
+                <a href="#" class="btn">Shop Now</a>
+            </div>
+
+        </section>
+
+        <section class="products" id="products">
+            <h1 class="heading">Our<span> Catalog</span></h1>
+            <div class="box-container">
+                <div class="box">
+                    <div class="image">
+                        <img src="4images/flowervase.webp" alt="flowervase">
+                        <div class="icons">
+                            <a href="#" class="cart-btn"> add to cart</a>
+                        </div>
+                    </div>
+                    <div class="content">
+                        <h3>Flower Vase</h3>
+                        <div class="price">€16.99<span>€21.49</span></div>
+                    </div>
+                </div>
+
+                <div class="box">
+                    <div class="image">
+                        <img src="/Team28/images/angelina-jollivet-mNEpmNiFdXs-unsplash.jpg" alt="">
+                        <div class="icons">
+                            <a href="#" class="cart-btn"> add to cart</a>
+                        </div>
+                    </div>
+                    <div class="content">
+                        <h3>lilly</h3>
+                        <div class="price">€5.99<span>€10.99</span></div>
+                    </div>
+                </div>
+
+                <div class="box">
+                    <div class="image">
+                        <img src="images/Tulips.jpg" alt="">
+                        <div class="icons">
+                            <a href="#" class="cart-btn"> add to cart</a>
+                        </div>
+                    </div>
+                    <div class="content">
+                        <h3>Tulips</h3>
+                        <div class="price">€6.99<span>€10.99</span></div>
+                    </div>
+                </div>
+
+                <div class="box">
+                    <div class="image">
+                        <img src="images/img3.jpg" alt="">
+                        <div class="icons">
+                            <a href="#" class="cart-btn"> add to cart</a>
+                        </div>
+                    </div>
+                    <div class="content">
+                        <h3>Roses</h3>
+                        <div class="price">€16.99<span>€21.49</span></div>
+                    </div>
+                </div>
+
+
+                <div class="box">
+                    <div class="image">
+                        <img src="images/img2.jpg" alt="">
+                        <div class="icons">
+                            <a href="#" class="cart-btn"> add to cart</a>
+                        </div>
+                    </div>
+                    <div class="content">
+                        <h3>White Roses</h3>
+                        <div class="price">€16.99<span>€21.49</span></div>
+                    </div>
+                </div>
+            </div>
+        </section>    
+
+
+<?php
+include 'includes/footer.php';
+?>
