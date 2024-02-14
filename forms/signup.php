9946b80515eb2711e6480c4d8b9ab84ec275aa4a
Nelson Njoku <113510892+Acoolwolf96@users.noreply.github.com>
2024-02-14 13:44:14 +0200

    more changes

--- forms/signup.php
+++ forms/signup.php
@@ -7,18 +7,22 @@
 </head>
 <body>
 <h2>Signup Form</h2>
-    <form action="#" method="post">
+    <form action="/Team28/process.php" method="post">
         <div class="form-group">
             <label for="fullname">Full Name</label>
-            <input type="text" id="fullname" name="fullname" required>
+            <input type="text" id="fullname" name="name" required>
         </div>
         <div class="form-group">
             <label for="email">Email</label>
             <input type="email" id="email" name="email" required>
         </div>
         <div class="form-group">
-            <label for="password">Password</label>
-            <input type="password" id="password" name="password" required>
+            <label for="Phone">Phone</label>
+            <input type="Phone" id="Phone" name="phone" required>
+        </div>
+        <div class="form-group">
+            <label for="address">Address</label>
+            <input type="address" id="address" name="address" required>
         </div>
         <div class="form-group">
             <input type="submit" value="Sign Up">
--- includes/connect_to_db.php
+++ includes/connect_to_db.php
@@ -1,11 +1,11 @@
 <?php
-$hostname = 'db'; // Database host
-$username = 'root'; // Database username
+$servername = 'phptask-db-1'; // Database host --Change servername to your servername
+$username = 'Team28'; // Database username
 $password = 'password'; // Database password
 $database = 'Team28'; // Database name
 
 // Create a database connection
-$conn = new mysqli($hostname, $username, $password, $database);
+$conn = new mysqli($servername, $username, $password, $database);
 
 // Check for connection errors
 if ($conn->connect_error) {
